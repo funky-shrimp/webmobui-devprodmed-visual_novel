fd840f9748a3f8d239f40310c7a2525a8de764d4
Funky Shrimp <thibaud.nuss@gmail.com>
2025-04-28 16:02:21 +0200

    Modified API routes to be on date with main branch

--- resources/js/lib/StoriesManager.js
+++ resources/js/lib/StoriesManager.js
@@ -33,7 +33,7 @@ export function createStory(story) {
 }
 
 export function getStoryChapters(id) {
-    const { data, error, isLoading } = useFetchJson("/api/chapters/story/" + id);
+    const { data, error, isLoading } = useFetchJson("/api/stories/" + id+"/chapters");
     return { data, error, isLoading };
 }
 
